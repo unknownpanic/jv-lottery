f0304ea91bc31da193a30ed662706290668b5be2
maxima <maxima.at.work@gmail.com>
2025-09-17 14:35:09 +0300

    created lottery-ball system

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball[] arrayOfBalls = new Ball[3];
+
+        for (int i = 0; i < arrayOfBalls.length; i++) {
+            arrayOfBalls[i] = lottery.getRandomBall();
+            System.out.println(arrayOfBalls[i].toString());
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        int randomNumber = new Random().nextInt(100);
+        String randomColor = ColorSupplier.getRandomColors();
+
+        return new Ball(randomColor, randomNumber);
+    }
+}
