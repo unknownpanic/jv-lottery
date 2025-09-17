a16570b040a85a0adc8c45357fa4557b73467925
maxima <maxima.at.work@gmail.com>
2025-09-17 16:07:02 +0300

    hot-fix

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+    private Colors[] values = Colors.values();
 
-    public String getRandomColors() {
-        int index = random.nextInt(Colors.values().length);
-        Colors color = Colors.values()[index];
-        return color.toString();
+    public String getRandomColor() {
+        int index = random.nextInt(values.length);
+        Colors color = values[index];
+        return color.name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public Ball getRandomBall() {
-        int randomNumber = new Random().nextInt(100);
-        ColorSupplier colorSupplier = new ColorSupplier();
-        String randomColor = colorSupplier.getRandomColors();
+    private static final int MAX_NUMBER = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
-        return new Ball(randomColor, randomNumber);
+    public Ball getRandomBall() {
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER + 1));
     }
 }
